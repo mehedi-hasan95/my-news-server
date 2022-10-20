efb0b5f5ad8980da81372e40effd6335278837b6
Mehedi Titu <mh.me.mehedi@gmail.com>
2022-10-20 10:52:45 +0600

    Compleate news and category api

--- index.js
+++ index.js
@@ -13,10 +13,26 @@ app.get('/', (req, res) => {
 })
 
 app.get('/categories', (req, res) => {
-    res.send(categories);
+  res.send(categories);
 })
-app.get('/news', (req, res) => {
-    res.send(news);
+
+
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  if (id === '08') {
+    res.send(news)
+  } else {
+    const categoryNews = news.filter(n => n.category_id === id);
+    res.send(categoryNews);
+  }
+})
+
+
+
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews);
 })
 
 app.listen(port, () => {
